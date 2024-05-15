53592f48a3d4a4ebd519e69336cec15a1378d6dc
Flavia Ramos <flaviaccraamos.fr@gmail.com>
2024-05-14 22:02:52 -0300

    Front Tela Login

--- app/src/main/java/com/example/gafa_pjo/MainActivity.kt
+++ app/src/main/java/com/example/gafa_pjo/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.gafa_pjo
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.example.gafa_pjo.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = Color.parseColor("#FFFFFF")
+
+        binding.btnEntrar.setOnClickListener {
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when{
+                email.isEmpty() -> {
+                    binding.editEmail.error = "Preencha o E-mail!"
+                }
+                senha.isEmpty() -> {
+                    binding.editSenha.error = "Preencha a Senha!"
+                }
+                !email.contains("gmail.com") -> {
+                    val snackbar = Snackbar.make(it,"E-mail inválido", Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+                }
+                senha.length <= 5 -> {
+                    val snackbar = Snackbar.make(it,"A senha precisa ter pelo menos 6 caracteres!", Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+                }
+                else ->{
+                    login(it)
+                }
+            }
+        }
+    }
+    private fun login(view: View){
+
+        val progressbar = binding.progressbar
+        progressbar.visibility = View.VISIBLE
+
+        binding.btnEntrar.isEnabled = false
+        binding.btnEntrar.setTextColor(Color.parseColor("#FFFFFF"))
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val snackbar = Snackbar.make(view,"Login efetuado com sucesso!", Snackbar.LENGTH_SHORT)
+            snackbar.show()
+        },3000)
     }
 }
